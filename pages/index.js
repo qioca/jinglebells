abd839a8caf6a39ac38f70696f08e77fe3653461
qioca <72082109+qioca@users.noreply.github.com>
2020-10-03 23:36:40 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,42 +23,10 @@ export default function Home() {
             <h3>jingle all the way!</h3>
             <p>Here we.. on an HNS driven sleigh</p>
           </a>
-
-          <!--<a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>blockchain</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>decentralized</h3>
-            <p></p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              something else here
-            </p>
-          </a>
+    
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          finally something here.
-        </a>
-      </footer>
-    </div>-->
+    
+    </div>
   )
 }
